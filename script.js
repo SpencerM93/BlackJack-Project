907a3075224056febf25dc81cd4c49d562c67b0d
augdog97 <50853964+augdog97@users.noreply.github.com>
2020-05-15 01:45:58 -0400

    Add files via upload
    Added some starting script for variables

---
+++ script.js
@@ -0,0 +1,14 @@
+//   Deck Variables
+let dealer  = document.getElementById('dealer');
+let deck    = document.getElementById('deck');
+let user    = document.getElementById('user');
+
+//Button Variables
+let deal  = document.getElementById('deal');
+let hold  = document.getElementById('hold');
+let hit   = document.getElementById('hit');
+
+//Deck Array
+const deck = [];
+
+//Math.random function 
